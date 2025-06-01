8a880b826b0732f6643d99b35ff4e9592afd627e
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2025-05-31 20:51:22 -0500

    Format code

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const levelText = document.getElementById('level');
-const xpText = document.getElementById('xp');
-const earnXpButton = document.getElementById('earnXpButton');
+const levelText = document.getElementById("level");
+const xpText = document.getElementById("xp");
+const earnXpButton = document.getElementById("earnXpButton");
 let level = 1;
 let xp = 0;
 let xpNeeded = 1;
 
-earnXpButton.addEventListener('click', () => {
+earnXpButton.addEventListener("click", () => {
   xp += 1;
   if (xp >= xpNeeded) {
     level++;
