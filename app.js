3bdd6d7256f1962d4bfeae32178e60be0276c26b
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2025-05-31 21:27:51 -0500

    Add progress bar

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 const levelText = document.getElementById("level");
 const xpText = document.getElementById("xp");
 const earnXpButton = document.getElementById("earnXpButton");
+const progressBar = document.getElementById("progressBar");
 let level = 1;
 let xp = 0;
 let xpNeeded = 1;
 
 earnXpButton.addEventListener("click", () => {
   xp += 1;
+  progressBar.style.width = `${(xp / xpNeeded) * 100}%`;
+  xpText.textContent = xp;
   if (xp >= xpNeeded) {
     level++;
     xpNeeded = level;
     levelText.textContent = level;
     xp = 0;
     xpText.textContent = xp;
+    progressBar.style.width = "0%";
   }
 });
