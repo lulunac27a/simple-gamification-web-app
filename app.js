f76d28adff44705332e315d04471cab1e7a8e424
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2025-05-31 20:49:08 -0500

    Initialize web app

---
+++ app.js
@@ -0,0 +1,17 @@
+const levelText = document.getElementById('level');
+const xpText = document.getElementById('xp');
+const earnXpButton = document.getElementById('earnXpButton');
+let level = 1;
+let xp = 0;
+let xpNeeded = 1;
+
+earnXpButton.addEventListener('click', () => {
+  xp += 1;
+  xpText.textContent = xp;
+  if (xp >= xpNeeded) {
+    level++;
+    xpNeeded = level;
+    levelText.textContent = level;
+    xp = 0;
+  }
+});
